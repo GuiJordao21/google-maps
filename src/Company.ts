f0b7d79e722dca7e2ce0cdcedbba1a2b9479e0a6
GuiJordao <guilherme@netpark.com.br>
2020-05-24 13:30:44 -0300

    first commit

---
+++ src/Company.ts
@@ -0,0 +1,27 @@
+import * as faker from "faker";
+
+export class Company {
+  name: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `
+    <div>
+      <h1>Company name: ${this.name}</h1>
+      <h3>Company catch phrase: ${this.catchPhrase}</h3>
+    </div>`;
+  }
+}
---
+++ src/User.ts
@@ -0,0 +1,24 @@
+import * as faker from "faker";
+
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `
+    <div>
+      <h1>User name: ${this.name}</h1>
+    </div>`;
+  }
+}
